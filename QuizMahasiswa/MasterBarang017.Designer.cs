1a942d029c8b0a29385b868bd5cbca6be3440774
Muhammad Irfan Aditya <0320200017@polman.astra.ac.id>
2021-05-27 11:55:33 +0700

    update label judul master barang

--- QuizMahasiswa/MasterBarang017.Designer.cs
+++ QuizMahasiswa/MasterBarang017.Designer.cs
@@ -37,82 +37,82 @@
             this.txtHarga = new System.Windows.Forms.TextBox();
             this.txtNamaBarang = new System.Windows.Forms.TextBox();
             this.dt = new System.Windows.Forms.DataGridView();
+            this.idbarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namabarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hargaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stokDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namasupplierDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tblbarangBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.quizDataSet = new QuizMahasiswa.QuizDataSet();
             this.btnSave = new System.Windows.Forms.Button();
             this.lbl2 = new System.Windows.Forms.Label();
             this.lbl1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.quizDataSet = new QuizMahasiswa.QuizDataSet();
-            this.tblbarangBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.tbl_barangTableAdapter = new QuizMahasiswa.QuizDataSetTableAdapters.tbl_barangTableAdapter();
-            this.idbarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.namabarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.hargaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.stokDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.namasupplierDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.quizDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblbarangBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quizDataSet)).BeginInit();
             this.SuspendLayout();
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(256, 117);
+            this.txtID.Location = new System.Drawing.Point(288, 146);
             this.txtID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(226, 22);
+            this.txtID.Size = new System.Drawing.Size(254, 26);
             this.txtID.TabIndex = 53;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(88, 117);
+            this.label2.Location = new System.Drawing.Point(99, 146);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.Size = new System.Drawing.Size(82, 20);
             this.label2.TabIndex = 52;
             this.label2.Text = "ID Barang";
             // 
             // txtRp
             // 
             this.txtRp.AutoSize = true;
-            this.txtRp.Location = new System.Drawing.Point(252, 221);
+            this.txtRp.Location = new System.Drawing.Point(284, 276);
             this.txtRp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.txtRp.Name = "txtRp";
-            this.txtRp.Size = new System.Drawing.Size(26, 17);
+            this.txtRp.Size = new System.Drawing.Size(30, 20);
             this.txtRp.TabIndex = 51;
             this.txtRp.Text = "Rp";
             // 
             // txtNamaSupplier
             // 
-            this.txtNamaSupplier.Location = new System.Drawing.Point(256, 318);
-            this.txtNamaSupplier.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNamaSupplier.Location = new System.Drawing.Point(288, 398);
+            this.txtNamaSupplier.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtNamaSupplier.Name = "txtNamaSupplier";
-            this.txtNamaSupplier.Size = new System.Drawing.Size(226, 22);
+            this.txtNamaSupplier.Size = new System.Drawing.Size(254, 26);
             this.txtNamaSupplier.TabIndex = 50;
             // 
             // txtStok
             // 
-            this.txtStok.Location = new System.Drawing.Point(256, 268);
-            this.txtStok.Margin = new System.Windows.Forms.Padding(4);
+            this.txtStok.Location = new System.Drawing.Point(288, 335);
+            this.txtStok.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtStok.Name = "txtStok";
-            this.txtStok.Size = new System.Drawing.Size(226, 22);
+            this.txtStok.Size = new System.Drawing.Size(254, 26);
             this.txtStok.TabIndex = 49;
             // 
             // txtHarga
             // 
-            this.txtHarga.Location = new System.Drawing.Point(288, 217);
-            this.txtHarga.Margin = new System.Windows.Forms.Padding(4);
+            this.txtHarga.Location = new System.Drawing.Point(324, 271);
+            this.txtHarga.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtHarga.Name = "txtHarga";
-            this.txtHarga.Size = new System.Drawing.Size(194, 22);
+            this.txtHarga.Size = new System.Drawing.Size(218, 26);
             this.txtHarga.TabIndex = 48;
             // 
             // txtNamaBarang
             // 
-            this.txtNamaBarang.Location = new System.Drawing.Point(256, 167);
-            this.txtNamaBarang.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNamaBarang.Location = new System.Drawing.Point(288, 209);
+            this.txtNamaBarang.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtNamaBarang.Name = "txtNamaBarang";
-            this.txtNamaBarang.Size = new System.Drawing.Size(226, 22);
+            this.txtNamaBarang.Size = new System.Drawing.Size(254, 26);
             this.txtNamaBarang.TabIndex = 47;
             // 
             // dt
@@ -128,19 +128,64 @@
             this.stokDataGridViewTextBoxColumn,
             this.namasupplierDataGridViewTextBoxColumn});
             this.dt.DataSource = this.tblbarangBindingSource;
-            this.dt.Location = new System.Drawing.Point(92, 412);
-            this.dt.Margin = new System.Windows.Forms.Padding(4);
+            this.dt.Location = new System.Drawing.Point(104, 515);
+            this.dt.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dt.Name = "dt";
             this.dt.ReadOnly = true;
-            this.dt.Size = new System.Drawing.Size(600, 185);
+            this.dt.Size = new System.Drawing.Size(675, 231);
             this.dt.TabIndex = 46;
+            // 
+            // idbarangDataGridViewTextBoxColumn
+            // 
+            this.idbarangDataGridViewTextBoxColumn.DataPropertyName = "id_barang";
+            this.idbarangDataGridViewTextBoxColumn.HeaderText = "id_barang";
+            this.idbarangDataGridViewTextBoxColumn.Name = "idbarangDataGridViewTextBoxColumn";
+            this.idbarangDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // namabarangDataGridViewTextBoxColumn
+            // 
+            this.namabarangDataGridViewTextBoxColumn.DataPropertyName = "nama_barang";
+            this.namabarangDataGridViewTextBoxColumn.HeaderText = "nama_barang";
+            this.namabarangDataGridViewTextBoxColumn.Name = "namabarangDataGridViewTextBoxColumn";
+            this.namabarangDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // hargaDataGridViewTextBoxColumn
+            // 
+            this.hargaDataGridViewTextBoxColumn.DataPropertyName = "harga";
+            this.hargaDataGridViewTextBoxColumn.HeaderText = "harga";
+            this.hargaDataGridViewTextBoxColumn.Name = "hargaDataGridViewTextBoxColumn";
+            this.hargaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // stokDataGridViewTextBoxColumn
+            // 
+            this.stokDataGridViewTextBoxColumn.DataPropertyName = "stok";
+            this.stokDataGridViewTextBoxColumn.HeaderText = "stok";
+            this.stokDataGridViewTextBoxColumn.Name = "stokDataGridViewTextBoxColumn";
+            this.stokDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // namasupplierDataGridViewTextBoxColumn
+            // 
+            this.namasupplierDataGridViewTextBoxColumn.DataPropertyName = "nama_supplier";
+            this.namasupplierDataGridViewTextBoxColumn.HeaderText = "nama_supplier";
+            this.namasupplierDataGridViewTextBoxColumn.Name = "namasupplierDataGridViewTextBoxColumn";
+            this.namasupplierDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // tblbarangBindingSource
+            // 
+            this.tblbarangBindingSource.DataMember = "tbl_barang";
+            this.tblbarangBindingSource.DataSource = this.quizDataSet;
+            // 
+            // quizDataSet
+            // 
+            this.quizDataSet.DataSetName = "QuizDataSet";
+            this.quizDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(92, 368);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Location = new System.Drawing.Point(104, 460);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.Size = new System.Drawing.Size(112, 35);
             this.btnSave.TabIndex = 45;
             this.btnSave.Text = "Simpan";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -149,40 +194,40 @@
             // lbl2
             // 
             this.lbl2.AutoSize = true;
-            this.lbl2.Location = new System.Drawing.Point(88, 318);
+            this.lbl2.Location = new System.Drawing.Point(99, 398);
             this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl2.Name = "lbl2";
-            this.lbl2.Size = new System.Drawing.Size(101, 17);
+            this.lbl2.Size = new System.Drawing.Size(113, 20);
             this.lbl2.TabIndex = 44;
             this.lbl2.Text = "Nama Supplier";
             // 
             // lbl1
             // 
             this.lbl1.AutoSize = true;
-            this.lbl1.Location = new System.Drawing.Point(88, 268);
+            this.lbl1.Location = new System.Drawing.Point(99, 335);
             this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(36, 17);
+            this.lbl1.Size = new System.Drawing.Size(42, 20);
             this.lbl1.TabIndex = 43;
             this.lbl1.Text = "Stok";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(88, 217);
+            this.label5.Location = new System.Drawing.Point(99, 271);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.Size = new System.Drawing.Size(53, 20);
             this.label5.TabIndex = 42;
             this.label5.Text = "Harga";
             // 
             // label
             // 
             this.label.AutoSize = true;
-            this.label.Location = new System.Drawing.Point(88, 167);
+            this.label.Location = new System.Drawing.Point(99, 209);
             this.label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(95, 17);
+            this.label.Size = new System.Drawing.Size(107, 20);
             this.label.TabIndex = 41;
             this.label.Text = "Nama Barang";
             // 
@@ -190,61 +235,21 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(191, 27);
+            this.label1.Location = new System.Drawing.Point(215, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(317, 39);
+            this.label1.Size = new System.Drawing.Size(465, 46);
             this.label1.TabIndex = 40;
-            this.label1.Text = "Form Input Barang";
-            // 
-            // quizDataSet
-            // 
-            this.quizDataSet.DataSetName = "QuizDataSet";
-            this.quizDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // tblbarangBindingSource
-            // 
-            this.tblbarangBindingSource.DataMember = "tbl_barang";
-            this.tblbarangBindingSource.DataSource = this.quizDataSet;
+            this.label1.Text = "FORM INPUT BARANG";
             // 
             // tbl_barangTableAdapter
             // 
             this.tbl_barangTableAdapter.ClearBeforeFill = true;
             // 
-            // idbarangDataGridViewTextBoxColumn
-            // 
-            this.idbarangDataGridViewTextBoxColumn.DataPropertyName = "id_barang";
-            this.idbarangDataGridViewTextBoxColumn.HeaderText = "id_barang";
-            this.idbarangDataGridViewTextBoxColumn.Name = "idbarangDataGridViewTextBoxColumn";
-            // 
-            // namabarangDataGridViewTextBoxColumn
-            // 
-            this.namabarangDataGridViewTextBoxColumn.DataPropertyName = "nama_barang";
-            this.namabarangDataGridViewTextBoxColumn.HeaderText = "nama_barang";
-            this.namabarangDataGridViewTextBoxColumn.Name = "namabarangDataGridViewTextBoxColumn";
-            // 
-            // hargaDataGridViewTextBoxColumn
-            // 
-            this.hargaDataGridViewTextBoxColumn.DataPropertyName = "harga";
-            this.hargaDataGridViewTextBoxColumn.HeaderText = "harga";
-            this.hargaDataGridViewTextBoxColumn.Name = "hargaDataGridViewTextBoxColumn";
-            // 
-            // stokDataGridViewTextBoxColumn
-            // 
-            this.stokDataGridViewTextBoxColumn.DataPropertyName = "stok";
-            this.stokDataGridViewTextBoxColumn.HeaderText = "stok";
-            this.stokDataGridViewTextBoxColumn.Name = "stokDataGridViewTextBoxColumn";
-            // 
-            // namasupplierDataGridViewTextBoxColumn
-            // 
-            this.namasupplierDataGridViewTextBoxColumn.DataPropertyName = "nama_supplier";
-            this.namasupplierDataGridViewTextBoxColumn.HeaderText = "nama_supplier";
-            this.namasupplierDataGridViewTextBoxColumn.Name = "namasupplierDataGridViewTextBoxColumn";
-            // 
             // MasterBarang017
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(781, 624);
+            this.ClientSize = new System.Drawing.Size(879, 780);
             this.Controls.Add(this.txtID);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtRp);
@@ -259,12 +264,13 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MasterBarang017";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.MasterBarang017_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.quizDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblbarangBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quizDataSet)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
